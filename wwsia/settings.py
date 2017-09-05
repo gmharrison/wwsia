9011c969ac082d0040e57f06b2fa25ef75a173ea
Gabrielle Harrison <gabrielle@codesters.com>
2017-09-05 18:37:32 -0400

    troubleshooting s3 access

--- wwsia/settings.py
+++ wwsia/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wwsia'
+    'wwsia',
+    'storages'
 ]
 
 MIDDLEWARE = [
