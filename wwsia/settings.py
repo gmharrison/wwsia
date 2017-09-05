cfbf3b3a39be549b218f94fb11206f9c2465352f
Gabrielle Harrison <gabrielle@codesters.com>
2017-09-05 18:34:16 -0400

    troubleshooting s3 access

--- wwsia/settings.py
+++ wwsia/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 
 INSTALLED_APPS = [
